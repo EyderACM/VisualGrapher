8402dd2ad540f2023a633a8a7aee7bed495423dc
EyderACM <eyderacm@gmail.com>
2020-05-23 19:12:26 -0500

    [Impl] MatrixGraph searches

--- src/graphs/MatrixGraph.java
+++ src/graphs/MatrixGraph.java
@@ -3,9 +3,7 @@ package graphs;
 import schema.Graph;
 import schema.Node;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MatrixGraph<T> extends Graph {
 
@@ -21,9 +19,20 @@ public class MatrixGraph<T> extends Graph {
         MatrixGraph MG = new MatrixGraph();
 
         try {
-            MG.newVertex("Patito");
-            MG.newVertex("patitote");
-            MG.newVertex("patitoteee");
+            MG.newVertex("A");
+            MG.newVertex("B");
+            MG.newVertex("C");
+            MG.newVertex("D");
+            MG.newVertex("E");
+            MG.newVertex("X");
+            MG.newVertex("P");
+
+            MG.addArch("A", "B"); MG.addArch("A", "C"); MG.addArch("A", "X");
+            MG.addArch("B", "D"); MG.addArch("B", "E");
+            MG.addArch("C", "D"); MG.addArch("C", "X");
+            MG.addArch("D", "E"); MG.addArch("D", "P");
+            MG.addArch("X", "P");
+            System.out.println(MG.DFS("C"));
 
         }catch (Exception e){
             System.out.println(e);
@@ -102,21 +111,153 @@ public class MatrixGraph<T> extends Graph {
 
     @Override
     public boolean BFS(String nodeName) {
-        return false;
-    }
+        if(nodeList.size() == 0) return false;
+        Queue<Integer> toVisit = new LinkedList();
+        HashSet<Integer> visited = new HashSet();
 
-    @Override
-    public boolean DFS(String nodeName) {
-        return false;
+        toVisit.add(0);
+        visited.add(0);
+        while(visited.size() < nodeList.size()){
+            // To consider lonely nodes
+            if(toVisit.isEmpty()){
+                for (Node node : nodeList){
+                    int nodePos = nodePosition(node);
+                    if(!visited.contains(nodePos)) {
+                        visited.add(nodePos);
+                        toVisit.add(nodePos);
+                    }
+                }
+            }
+
+            int lastToVisit = toVisit.peek();
+            int pos = 0;
+            for(Integer adjacentRel : adjacentMatrix.get(lastToVisit)){
+                if(!visited.contains(pos) && adjacentRel == 1){
+                    visited.add(pos);
+                    toVisit.add(pos);
+                }
+                pos++;
+            }
+            toVisit.remove();
+        }
+        return visited.contains(nodePosition(new Node(nodeName, nodeName)));
     }
 
     @Override
     public String breadthFirstTraversal(String partingNode) throws NodeNotFound {
-        return null;
+        int partingNodePos = nodePosition(new Node(partingNode, partingNode));
+        if(partingNodePos == -1) throw new NodeNotFound();
+        Queue<Integer> toVisit = new LinkedList();
+        HashSet<Integer> visited = new HashSet();
+        String result = partingNode;
+
+        toVisit.add(partingNodePos);
+        visited.add(partingNodePos);
+        while(visited.size() < nodeList.size()){
+            // To consider lonely nodes
+            if(toVisit.isEmpty()){
+                for (Node node : nodeList){
+                    int nodePos = nodePosition(node);
+                    if(!visited.contains(nodePos)) {
+                        visited.add(nodePos);
+                        toVisit.add(nodePos);
+                        result += " -> " + node.getNodeName();
+                    }
+                }
+            }
+
+            int lastToVisit = toVisit.peek();
+            int pos = 0;
+            for(Integer adjacentRel : adjacentMatrix.get(lastToVisit)){
+                if(!visited.contains(pos) && adjacentRel == 1){
+                    visited.add(pos);
+                    result += " -> " + nodeList.get(pos).getNodeName();
+                    toVisit.add(pos);
+                }
+                pos++;
+            }
+            toVisit.remove();
+        }
+        return result;
+    }
+
+    @Override
+    public boolean DFS(String nodeName) {
+        if(nodeList.size() == 0) return false;
+        Stack<Integer> toVisit = new Stack<Integer>();
+        HashSet<Integer> visited = new HashSet();
+
+        toVisit.add(0);
+        visited.add(0);
+        while(visited.size() < nodeList.size()){
+            boolean found = false;
+
+            // To consider lonely nodes
+            if(toVisit.isEmpty()){
+                for (Node node : nodeList){
+                    int nodePos = nodePosition(node);
+                    if(!visited.contains(nodePos) && !found) {
+                        visited.add(nodePos);
+                        toVisit.add(nodePos);
+                        found = true;
+                    }
+                }
+            }
+
+            int lastToVisit = toVisit.peek();
+            int pos = 0;
+            for(Integer adjacentRel : adjacentMatrix.get(lastToVisit)){
+                if(!visited.contains(pos) && !found && adjacentRel == 1){
+                    visited.add(pos);
+                    toVisit.add(pos);
+                    found = true;
+                }
+                pos++;
+            }
+            if(!found) toVisit.pop();
+        }
+        return visited.contains(nodePosition(new Node(nodeName, nodeName)));
     }
 
     @Override
     public String deepFirstTraversal(String partingNode) throws NodeNotFound {
-        return null;
+        int partingNodePos = nodePosition(new Node(partingNode, partingNode));
+        if(partingNodePos == -1) throw new NodeNotFound();
+        Stack<Integer> toVisit = new Stack<Integer>();
+        HashSet<Integer> visited = new HashSet();
+        String result = partingNode;
+
+        toVisit.add(partingNodePos);
+        visited.add(partingNodePos);
+        while(visited.size() < nodeList.size()){
+            boolean found = false;
+
+            // To consider lonely nodes
+            if(toVisit.isEmpty()){
+                for (Node node : nodeList){
+                    int nodePos = nodePosition(node);
+                    if(!visited.contains(nodePos) && !found) {
+                        visited.add(nodePos);
+                        toVisit.add(nodePos);
+                        result += " -> " + node.getNodeName();
+                        found = true;
+                    }
+                }
+            }
+
+            int lastToVisit = toVisit.peek();
+            int pos = 0;
+            for(Integer adjacentRel : adjacentMatrix.get(lastToVisit)){
+                if(!visited.contains(pos) && !found && adjacentRel == 1){
+                    visited.add(pos);
+                    toVisit.add(pos);
+                    result += " -> " + nodeList.get(pos).getNodeName();
+                    found = true;
+                }
+                pos++;
+            }
+            if(!found) toVisit.pop();
+        }
+        return result;
     }
 }
